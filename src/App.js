479cbce582901245306c8d98ff86c6aee7c2fe2e
Shubham Aggarwal <shubham.aggarwal@wingify.com>
2021-03-17 11:08:20 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components'
+
+const Wrapper = styled.section`
+  padding: 3em;
+  background: papayawhip;
+`;
+
+const NewWrapper = styled.section`
+  padding: 9em;
+  background: red;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+        <div className="App">
+            <header className="App-header">
+                Learn kjbfhjbjhbdjhbhjk
+            </header>
+            <NewWrapper>
+                Hello
+            </NewWrapper>
+        </div>
+    </Wrapper>
   );
 }
 
