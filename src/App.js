1887dd7289c4b4cf5d55562bcdf4c32533750ba3
Shubham Aggarwal <shubham.aggarwal@wingify.com>
2021-03-19 13:27:27 +0530

    init

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import './App.css';
 import styled from 'styled-components'
 
 const WrapperNew = styled.section`
+  & .blue {
+    color: blue;
+  }
+
+  & .red {
+    color: red;
+  }
+
   padding: 3em;
   background: ${props => props.primary ? "palevioletred" : "red"};
   font-size: ${props => props.big ? "18px" : "16px"};
@@ -13,11 +21,11 @@ function App() {
             <header className="App-header">
                 Learn react newly updated
             </header>
-            <WrapperNew primary>
-               first wrapper first
+            <WrapperNew primary big>
+                first wrapper first
             </WrapperNew>
-            <WrapperNew big>
-                First wrapper
+            <WrapperNew big className={"blue my-custom-shubham-class"}>
+                First wrapper with class
             </WrapperNew>
             <WrapperNew primary big>
                 Second Wrapper
