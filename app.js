a0306fe7659989b4601a6360f804d88ff395b234
Shubham Aggarwal <shubham.aggarwal@wingify.com>
2022-03-24 15:41:51 +0530

    init

--- app.js
+++ app.js
@@ -4,15 +4,14 @@ const path = require('path');
 const app = express()
 const port = process.env.PORT || 3000 // Heroku will need the PORT environment variable
 
-app.use((req, res, next) => {
-    res.set('Access-Control-Allow-Origin', ['*']);
-    res.set('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.set('Access-Control-Allow-Headers', 'Content-Type');
-    res.set('X-Frame-Options', 'DENY');
-    res.set('Access-Control-Expose-Headers', '*');
-    next();
-});
-
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'build'), {
+    setHeaders: function(res, path) {
+        res.set('Access-Control-Allow-Origin', ['*']);
+        res.set('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+        res.set('Access-Control-Allow-Headers', 'Content-Type');
+        res.set('X-Frame-Options', 'DENY');
+        res.set('Access-Control-Expose-Headers', '*');
+    }
+}));
 
 app.listen(port, () => console.log(`App is live on port ${port}!`))
