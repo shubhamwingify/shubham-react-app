02b319ea5dae5b2acce4e68fa8b26c6b0c6df31c
Shubham Aggarwal <shubham.aggarwal@wingify.com>
2022-03-27 22:36:25 +0530

    Aws

--- app.js
+++ app.js
@@ -4,14 +4,19 @@ const path = require('path');
 const app = express()
 const port = 3000 // Heroku will need the PORT environment variable
 
+const headers = function (res, path) {
+    res.set('Access-Control-Allow-Origin', ['*']);
+    res.set('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.set('Access-Control-Allow-Headers', 'Content-Type');
+    res.set('X-Frame-Options', 'DENY');
+    res.set('Access-Control-Expose-Headers', '*');
+};
+
+app.use(express.static(path.join(__dirname, 'build'), {
+    setHeaders: headers
+}));
 app.use('*', express.static(path.join(__dirname, 'build'), {
-    setHeaders: function(res, path) {
-        res.set('Access-Control-Allow-Origin', ['*']);
-        res.set('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-        res.set('Access-Control-Allow-Headers', 'Content-Type');
-        res.set('X-Frame-Options', 'DENY');
-        res.set('Access-Control-Expose-Headers', '*');
-    }
+    setHeaders: headers
 }));
 
 app.listen(port, () => console.log(`App is live on port ${port}!`))
