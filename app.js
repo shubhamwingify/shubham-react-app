f3a0a353a9824674f84c1f24bcc4cf50f0fd6147
Shubham Aggarwal <shubham.aggarwal@wingify.com>
2022-03-27 22:28:47 +0530

    aws

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express()
 const port = 3000 // Heroku will need the PORT environment variable
 
-app.use(express.static(path.join(__dirname, 'build'), {
+app.use('*', express.static(path.join(__dirname, 'build'), {
     setHeaders: function(res, path) {
         res.set('Access-Control-Allow-Origin', ['*']);
         res.set('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
